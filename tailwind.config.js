bb4ac3509f892e50fe66c935ee9af21cbe751eb7
Amadeus Kang <amadeuskang@gmail.com>
2024-06-10 12:41:22 -0400

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './templates/**/*.html',
-    './static/src/**/*.js',
+    './api/templates/**/*.html',
+    './api/static/src/**/*.js',
     './node_modules/flowbite/**/*.js',
   ],
   theme: {
